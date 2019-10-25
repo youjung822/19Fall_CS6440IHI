4efbce61444edfbb4be7d653bfdbbde8e58ca833
Andrew Lo <andrewlohc@gmail.com>
2019-10-25 00:41:54 -0700

    Fixing drone deployment

--- food-planner-back-end/src/main/java/hello/Application.java
+++ food-planner-back-end/src/main/java/hello/Application.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class Application {
     @RequestMapping("/")
     public String home() {
-        return "Hello world!";
+        return "Hello World";
     }
 
     @RequestMapping("*")
