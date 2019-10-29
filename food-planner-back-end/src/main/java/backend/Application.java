daa6b989fe9a957af2ff9579134330e403496922
Andrew Lo <andrewlohc@gmail.com>
2019-10-29 11:37:02 -0700

    API for returning a list of ingredients to for query.

--- food-planner-back-end/src/main/java/backend/Application.java
+++ food-planner-back-end/src/main/java/backend/Application.java
@@ -2,12 +2,19 @@ package backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @SpringBootApplication
 @RestController
 public class Application {
+    Ingredients ingredients = new Ingredients();
+
     @RequestMapping("/")
     public String home() {
         return "Hello Runtime Terror";
@@ -21,6 +28,12 @@ public class Application {
     @RequestMapping("/status")
     public String status() {
         return "Good.";
+    }
+
+    @RequestMapping(value = "/ingredients", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+    @ResponseBody
+    public List<String> listIngredients() {
+        return ingredients.get();
     }
 
     public static void main(String[] args) {
