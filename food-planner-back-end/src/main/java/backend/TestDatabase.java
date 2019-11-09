bdbaa4e5a23ee6abaa3dc5675afbf4067cff82f6
Andrew Lo <andrewlohc@gmail.com>
2019-11-09 03:33:55 -0500

    Fix postgresql jdbc imports

--- food-planner-back-end/src/main/java/backend/TestDatabase.java
+++ food-planner-back-end/src/main/java/backend/TestDatabase.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Component;
 
+import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -16,15 +17,6 @@ public class TestDatabase {
 
     @Autowired
     private Environment environment;
-
-//    @Value("${spring.datasource.url}")
-//    String url;
-//
-//    @Value("${spring.datasource.username}")
-//    String username;
-//
-//    @Value("${spring.datasource.password}")
-//    String password;
 
     public List<String> test() {
         List<String> result = new ArrayList<>();
@@ -37,24 +29,17 @@ public class TestDatabase {
 
         try (Connection connection = DriverManager.getConnection(url, username, password)) {
 
-            System.out.println("Java JDBC PostgreSQL Example");
-            // When this class first attempts to establish a connection, it automatically loads any JDBC 4.0 drivers found within
-            // the class path. Note that your application must manually load any JDBC drivers prior to version 4.0.
-            //          Class.forName("org.postgresql.Driver");
 
-            System.out.println("Connected to PostgreSQL database!");
+//            System.out.println("Connected to PostgreSQL database!");
             Statement statement = connection.createStatement();
-            System.out.println("Reading accounts records...");
+//            System.out.println("Reading accounts records...");
             ResultSet resultSet = statement.executeQuery("SELECT * FROM account;");
             while (resultSet.next()) {
                 System.out.printf("%-30.30s  %-30.30s%n", resultSet.getString("user_id"), resultSet.getString("username"));
                 result.add(resultSet.getString("user_id") + " " + resultSet.getString("username"));
             }
 
-        } /*catch (ClassNotFoundException e) {
-        System.out.println("PostgreSQL JDBC driver not found.");
-        e.printStackTrace();
-        }*/ catch (SQLException e) {
+        } catch (SQLException e) {
             System.out.println("Connection failure.");
             logger.debug("Connection failure.", e);
         }
